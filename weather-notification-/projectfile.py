e80bbb29d2dd85b8dad265c872a2c5a61877e87e
ChakrabortyDiya <crs.diya@gmail.com>
2024-05-12 13:25:42 +0530

    updated

--- weather-notification-/projectfile.py
+++ weather-notification-/projectfile.py
@@ -5,7 +5,7 @@ from win10toast import ToastNotifier
 n=ToastNotifier()
 def getdata(url):
      r = requests.get(url)  
-    return r.text # type: ignore
+     return r.text 
 htmldata=getdata("https://weather.com/en-IN/weather/today/l/c2c95ea60f4ef581b88c5c173d2ef178276d8fc26e6b39ad66f2e2e01f933c37")
 soup = BeautifulSoup(htmldata, 'html.parser')
 current_temp = soup.find_all("span", class_= "_-_-components-src-organism-CurrentConditions-CurrentConditions--tempValue--MHmYY") 
