9290371a322cc91426116f4b2f7db24d675cb2fe
ChakrabortyDiya <crs.diya@gmail.com>
2024-05-12 13:18:48 +0530

    updated

--- weather-notification-/projectfile.py
+++ weather-notification-/projectfile.py
@@ -6,3 +6,4 @@ n=ToastNotifier()
 def getdata(url):
      r = requests.get(url)  
     return r.text # type: ignore
+htmldata=getdata("https://weather.com/en-IN/weather/today/l/c2c95ea60f4ef581b88c5c173d2ef178276d8fc26e6b39ad66f2e2e01f933c37")
