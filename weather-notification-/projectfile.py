45cfede8634e3a91fd520cd879ca3e5e3aed940e
ChakrabortyDiya <crs.diya@gmail.com>
2024-05-12 13:28:50 +0530

    updated

--- weather-notification-/projectfile.py
+++ weather-notification-/projectfile.py
@@ -11,3 +11,5 @@ soup = BeautifulSoup(htmldata, 'html.parser')
 current_temp = soup.find_all("span", class_= "_-_-components-src-organism-CurrentConditions-CurrentConditions--tempValue--MHmYY") 
 chances_rain = soup.find_all("div", class_= "_-_-components-src-organism-CurrentConditions-CurrentConditions--precipValue--2aJSf") 
 temp = (str(current_temp)) 
+temp_rain = str(chances_rain) 
+result = "current_temp " + temp[128:-9] + "  in Kolkata West Bengal" + "\n" + temp_rain[131:-14] 
